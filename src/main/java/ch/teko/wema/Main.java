4b705cec8732854b3f5cf32ad1184bc714c354e8
tekoWeMa <mario.weilenmann@edu.teko.ch>
2022-12-11 11:41:07 +0100

    Initial Commit

---
+++ src/main/java/ch/teko/wema/Main.java
@@ -0,0 +1,82 @@
+package ch.teko.wema;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+// D:\Studium\IntellijProjekte\BrainfuckReadfile\src\main\resources\FileToRead.txt
+
+// Sauce: https://www.geeksforgeeks.org/java-program-to-read-a-file-to-string/
+
+public class Main {
+    public static void main(String[] args)
+        // idk for what this part is, i guess it handles an exception lol
+            throws IOException {
+        Path FileToRead
+                = Path.of("D:\\Studium\\IntellijProjekte\\BrainfuckReadfile\\src\\main\\resources\\FileToRead.txt");
+
+        //File.readString() method is used to read a text file and store it in a string
+        // calling files.readstring filetoreload, wich goes to path and reads the file
+
+        String str = Files.readString(FileToRead);
+
+        //System.out.println(str);
+
+        //create arraylist to store enums from string
+        ArrayList<OpcodeEnum> EnumList = new ArrayList<OpcodeEnum>();
+
+
+        //Iterate over String
+        // using simple for-loop
+        for (int i = 0; i < str.length(); i++) {
+
+            // System.out.println(str.charAt(i));
+
+            // Wert der Position i wird c zugewiesen.
+            char c = str.charAt(i);
+
+            // check if c is chararacter, create enum of that type
+            if (c == '>') {
+                OpcodeEnum code = OpcodeEnum.GRÖSSERALS;
+                EnumList.add(code);
+            } else if (c == '<') {
+                OpcodeEnum code = OpcodeEnum.KLEINERALS;
+                EnumList.add(code);
+            } else if (c == '+') {
+                OpcodeEnum code = OpcodeEnum.PLUS;
+                EnumList.add(code);
+            } else if (c == '-') {
+                OpcodeEnum code = OpcodeEnum.MINUS;
+                EnumList.add(code);
+            } else if (c == '.') {
+                OpcodeEnum code = OpcodeEnum.PUNKT;
+                EnumList.add(code);
+            } else if (c == ',') {
+                OpcodeEnum code = OpcodeEnum.KOMMA;
+                EnumList.add(code);
+            } else if (c == '[') {
+                OpcodeEnum code = OpcodeEnum.KLAMMERAUF;
+                EnumList.add(code);
+            } else if (c == ']') {
+                OpcodeEnum code = OpcodeEnum.KLAMMERZU;
+                EnumList.add(code);
+            }
+
+
+        }
+        System.out.println(EnumList);
+
+
+    }
+
+    public enum OpcodeEnum {
+
+        GRÖSSERALS, KLEINERALS, PLUS, MINUS, PUNKT, KOMMA, KLAMMERAUF, KLAMMERZU
+
+        //>, <, +, -, ., ",", [, ]
+    }
+
+}
+
+//TODO: enumlist.add(code); einmalig machen (for each loop?)
