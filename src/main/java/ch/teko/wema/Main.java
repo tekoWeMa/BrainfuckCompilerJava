0d54ab5a88007a02682c5c73126cbbeebc979460
tekoWeMa <98219925+tekoWeMa@users.noreply.github.com>
2023-01-22 16:48:56 +0100

    add bracketstack overflowing opening brackets print, add new todo

--- src/main/java/ch/teko/wema/Main.java
+++ src/main/java/ch/teko/wema/Main.java
@@ -45,7 +45,8 @@ public class Main {
                 case ']' -> {
                     EnumList.add(OpcodeEnum.KLAMMERZU);
                     if (bracketStack.isEmpty()) {
-                        System.out.println("Keis closing bracket ade Position: " + i);
+                        // Prints the position of the position of the closing bracket missing an opening bracket
+                        System.out.println("Keis corresponding zum closing bracket ade Position: " + i);
                     } else {
                         int beginIndex = bracketStack.pop();
                         for (BracketPair bp : bracketList) {
@@ -58,8 +59,14 @@ public class Main {
                 }
             }
         }
+        // Prints the index of the overflowing opening brackets
+        for (var stack:bracketStack){
+            System.out.println("Es Opening Bracket z vill ade position: " + stack);
+        }
+
+
        // Print enum list value
-        System.out.println(EnumList);
+       // System.out.println(EnumList);
 
         //print size
         for (int i = 0; i < EnumList.size(); i++) {
@@ -82,11 +89,9 @@ public class Main {
             this.begin = begin;
             this.end = end;
         }
-
         public int getBegin() {
             return begin;
         }
-
         public int getEnd() {
             return end;
         }
@@ -122,4 +127,5 @@ public class Main {
 // bei klasse sind getters und setters benötigt
 // array mit loops (sortieren)
 
-// TODO: klasse erstellen; wie kann ich die Werte des Begin und end ändern
+// TODO: alle Opcodes interpretieren ausser grösser und kleinerals, klammer auf und zu
+// TODO: danach klammern indexieren
